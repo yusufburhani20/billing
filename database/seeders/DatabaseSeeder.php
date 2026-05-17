c3bbbd88507e296ff614b1ed84dc855a388e950e
Yusuf Burhani <yusufburhani06@gmail.com>
2026-05-17 17:02:00 +0700

    Fix DatabaseSeeder to match refactored packages table schema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Router;
+use App\Models\MikrotikPool;
+use App\Models\MikrotikProfile;
+use App\Models\Package;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +20,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Admin User
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Admin Billing',
             'email' => 'admin@billing.com',
             'password' => bcrypt('password'),
@@ -24,7 +28,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Sample Router
-        \App\Models\Router::create([
+        $router = Router::create([
             'name' => 'Router Utama',
             'ip_address' => '192.168.88.1',
             'username' => 'admin',
@@ -32,20 +36,44 @@ class DatabaseSeeder extends Seeder
             'description' => 'Router pusat distribusi',
         ]);
 
+        // Sample Pool
+        $pool = MikrotikPool::create([
+            'name' => 'dhcp_pool_local',
+            'ranges' => '192.168.88.10-192.168.88.254',
+            'router_id' => $router->id,
+        ]);
+
+        // Sample Mikrotik Profiles
+        $profile10 = MikrotikProfile::create([
+            'name' => '10M',
+            'local_address' => '192.168.88.1',
+            'mikrotik_pool_id' => $pool->id,
+            'rate_limit' => '10M/10M',
+            'router_id' => $router->id,
+        ]);
+
+        $profile20 = MikrotikProfile::create([
+            'name' => '20M',
+            'local_address' => '192.168.88.1',
+            'mikrotik_pool_id' => $pool->id,
+            'rate_limit' => '20M/20M',
+            'router_id' => $router->id,
+        ]);
+
         // Sample Packages
-        \App\Models\Package::create([
+        Package::create([
             'name' => 'Hemat 10Mbps',
             'price' => 150000,
             'registration_fee' => 250000,
-            'speed_profile_name' => '10M',
+            'mikrotik_profile_id' => $profile10->id,
             'description' => 'Cocok untuk penggunaan rumahan ringan.',
         ]);
 
-        \App\Models\Package::create([
+        Package::create([
             'name' => 'Super 20Mbps',
             'price' => 250000,
             'registration_fee' => 250000,
-            'speed_profile_name' => '20M',
+            'mikrotik_profile_id' => $profile20->id,
             'description' => 'Cocok untuk streaming dan gaming.',
         ]);
     }
