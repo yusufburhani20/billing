5ceb93a011c086a95776036aee1bee45781b91bc
Yusuf Burhani <yusufburhani20@gmail.com>
2026-06-08 11:53:10 +0700

    build: compile frontend assets with system update option and ignore public/uploads

---
+++ public/build/assets/InputError-D1wWAtur.js
@@ -0,0 +1,1 @@
+import{t as e}from"./app-Bs3-_Yjq.js";var t=e();function n({message:e,className:n=``,...r}){return e?(0,t.jsx)(`p`,{...r,className:`text-sm text-red-600 `+n,children:e}):null}export{n as t};
